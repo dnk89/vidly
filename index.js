5a5fe387dbce94c438588d6c7ffcab7ba4c76890
dnk89 <dmitrij.kozlovskij@gmail.com>
2018-09-14 23:38:40 +0300

    Get all genres endpoint

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.send('Welcome to VIDLY app!');
+var genres = [{ id: 1, name: "comedy"}, { id: 2, name: "action"}];
+
+app.get('/api/genres', (req, res) => {
+    return res.send(genres);
 });
 
 const port = process.env.PORT || 3000;

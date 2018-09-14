692f819be247a178ea12677246b2bfeecc5c481b
dnk89 <dmitrij.kozlovskij@gmail.com>
2018-09-14 23:45:15 +0300

    Get genre by ID endpoint

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ app.get('/api/genres', (req, res) => {
     return res.send(genres);
 });
 
+app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    if (genre) {
+        return res.send(genre);
+    } else {
+        return res.status(404).send('Genre with given ID not found');
+    }
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {

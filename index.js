c2f1d4fc544c8017adb4e493b97264e300a196f7
dnk89 <dmitrij.kozlovskij@gmail.com>
2018-09-16 20:25:49 +0300

    Clean index from unnecessary rquire

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const Joi = require('joi');
 const express = require('express');
 const app = express();
 const genres = require('./routes/genres')
 
 app.use(express.json());
 app.use('/api/genres', genres);
-
 
 const port = process.env.PORT || 3000;
 
